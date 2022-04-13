de595cd51402545c3684185df11c377600739cda
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-04-13 09:40:13 +0530

    #include <stdio.h> int main() {   int i,j,k,n;   scanf("%d",&n);   for(i=1; i<=n; i++){
        for(j=1; j<=n-i; j++){
          printf(" ");
        }
    
        for(k=1; k<2*i; k++){
          printf("%d",i);
        }
    
        printf("
    ");
      }
    
      return 0;
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
+#include <stdio.h> 
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=n;j>0;j--)
-        {
-            printf("%d ",j);
-        }
-        printf("
-");
+  int i,j,k,n;
+  scanf("%d",&n);
+  for(i=1; i<=n; i++){
+
+    for(j=1; j<=n-i; j++){
+      printf(" ");
     }
+
+    for(k=1; k<2*i; k++){
+      printf("%d",i);
+    }
+
+    printf("
+");
+  }
+
+  return 0;
 }
